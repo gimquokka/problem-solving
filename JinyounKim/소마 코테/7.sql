31dbda70a532a9d771d78fec2dfe2c327a4c3480
Jin <gimquokka@gmail.com>
2021-02-27 16:41:06 +0900

    remove

--- JinyounKim/소마 코테/7.sql
+++
@@ -1,7 +0,0 @@
-SELECT o.buyer_id, o.buy_date, l.book_name, o.price 
-from library l, orderInfo o
-where 
-l.book_name = Looking with Elice 
-or  
-("2020-07-27"<= o.buy_date and o.buy_date <= "2020-07-31")
-order by l.buy_date;
--- JinyounKim/소마 코테/모의 테스트/1_Algorithms.py
+++
@@ -1,7 +0,0 @@
-def main():
-    x = input()
-    print('YES' if x.count('(') == x.count(')') else 'NO')
-
-
-if __name__ == "__main__":
-    main()
