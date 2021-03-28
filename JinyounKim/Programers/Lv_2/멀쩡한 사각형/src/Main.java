ccb9914cd846e2786fe03606c611796450959616
Jin <gimquokka@gmail.com>
2021-03-28 22:46:34 +0900

    0328 PS history

---
+++ JinyounKim/Programers/Lv_2/멀쩡한 사각형/src/Main.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String args[]){
+        Solution solution = new Solution();
+        System.out.println(Solution.solution(1, 2));
+    }
+}
+class Solution {
+    public long solution(int w, int h) {
+        long answer = 1;
+        return w+h;
+    }
+}
